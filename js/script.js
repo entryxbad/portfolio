65bd40f1b0320d57f8d18972305b6db8e5b3bfa4
Radmir <radmir_96@mail.ru>
2021-07-02 14:48:22 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    }
+    
+    testWebP(function (support) {
+    
+    if (support == true) {
+    document.querySelector('body').classList.add('webp');
+    }else{
+    document.querySelector('body').classList.add('no-webp');
+    }
+    });
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
+  const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+      anchor.addEventListener("click", function(event) {
+          event.preventDefault();
+          const blockID = anchor.getAttribute('href')
+          document.querySelector('' + blockID).scrollIntoView({
+              behavior: "smooth",
+              block: "start"
+          })
+      })
+  }
+  new WOW().init();
